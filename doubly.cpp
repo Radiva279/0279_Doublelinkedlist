a11cc4202747dedb7ae3a1f798ad50b4b2f64428
Radiva279 <radivagalih12@gmail.com>
2025-05-16 09:42:55 +0700

    menambahkan peulangan while

--- doubly.cpp
+++ doubly.cpp
@@ -57,4 +57,18 @@ public:
             return;
         }
 
+        // insert on between node
+        // step 8: locate posisition for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll number not allowed" << endl;
+            return;
+        }
+
 
